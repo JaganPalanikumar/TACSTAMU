b85aa8896f204d519aeeed893a4cafdb0299a3bb
FunniNam3 <nguyen.ev03@gmail.com>
2026-02-08 02:56:52 -0600

    Getting supabase working, and getting rid of .next directory cuz idk why its in the github

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,13 +1,23 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
+import { supabase } from "../../utils/supabase";
 
-type Data = {
-  name: string;
+type Team = {
+  max_members: number;
+  team_leader: string;
+  team_name: string;
 };
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>,
+  res: NextApiResponse<Team | String>,
 ) {
-  res.status(200).json({ name: "Ethan Nguyen" });
+  const { data: teams, error } = await supabase.from("Team").select();
+
+  if (error) {
+    console.error("Error fetching data: ", error.message);
+    throw error;
+  }
+
+  res.status(200).json(teams[0] as Team);
 }
