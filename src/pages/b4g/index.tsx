3d2b07e71a9e9832b7a5a907de4cfbf452939027
FunniNam3 <nguyen.ev03@gmail.com>
2026-02-12 17:30:23 -0600

    It looks and works a little better now

--- src/pages/b4g/index.tsx
+++ src/pages/b4g/index.tsx
@@ -4,89 +4,90 @@ import Image from "next/image";
 export default function Landing() {
   const { scrollY } = useScroll();
 
-  const moveCloud = useTransform(scrollY, [0, 1300], ["-30%", "-100%"]);
-  const moveBunny = useTransform(scrollY, [0, 800], ["0%", "-300%"]);
-  const moveBurst = useTransform(scrollY, [0, 1300], ["0%", "300%"]);
+  const moveCloud = useTransform(scrollY, [0, 2500], ["-55%", "-200%"]);
+  const moveBurst = useTransform(scrollY, [0, 200], ["10%", "100%"]);
+  const moveStar = useTransform(scrollY, [0, 500], ["30%", "100%"]);
   // TODO Finish a landing page
   return (
-    <div className="flex flex-col gap-3">
+    <div className="flex flex-col">
       <section className="relative h-[100dvh]">
         {/* Background */}
         <div className="absolute inset-0 -z-5">
           <motion.div
-            style={{ x: moveCloud }}
-            className="absolute bottom-0 left-0 w-[200vw] h-auto -translate-y-[50%]"
+            style={{ x: moveStar }}
+            className="absolute bottom-0 right-0 w-[2052.15px] h-auto invisible 2xl:visible"
           >
             <Image
-              src="/b4g/cloud with flower.svg"
-              alt="Cloud with Flower"
+              src="/b4g/star.svg"
+              alt="Shooting Star"
+              width={2052.15}
+              height={1065.68}
+              className=""
+              priority
+            />
+          </motion.div>
+          <motion.div
+            style={{ x: moveCloud, y: "40%" }}
+            className="absolute bottom-0 left-0 w-[2119.83px] h-auto"
+          >
+            <Image
+              src="/b4g/cloud bunny.svg"
+              alt="Cloud with Bunny"
               width={2119.83}
-              height={1199}
-              className="object-contain"
+              height={1587}
+              priority
+            />
+          </motion.div>
+          <motion.div
+            style={{ x: moveBurst }}
+            className="absolute bottom-0 right-0 w-fit h-auto"
+          >
+            <Image
+              src="/b4g/flower burst.svg"
+              alt="Flower and Burst"
+              width={605}
+              height={452}
               priority
             />
           </motion.div>
         </div>
 
         {/* Content */}
-        <div className="relative z-10 container mx-auto h-full flex items-center justify-between px-8">
-          <motion.div
-            style={{ x: moveBunny }}
-            className="relative top-0 left-[5%] w-[40vw] h-auto"
-          >
-            <Image
-              src="/b4g/cowboy bunny.svg"
-              alt="Cowboy Bunny"
-              width={391}
-              height={629.86}
-              priority
-            />
-          </motion.div>
-
-          <div className="block xl:hidden w-[30rem] relative right-0 top-[-30%]">
+        <div className="absolute z-10 inset-0">
+          <div className="absolute visible xl:invisible w-fit right-0 top-0  -translate-x-[20%] translate-y-[40%]">
             <Image
               src="/b4g/bunny logo.svg"
               alt="Bunny Logo"
               width={373.47}
               height={228.67}
               priority
-              className="h-auto -rotate-[6.69deg]"
+              className="-rotate-[6.69deg]"
             />
-            <h1 className="w-fit text-center m-auto text-3xl">build4good</h1>
+            <h1 className="w-fit text-center m-auto text-3xl">
+              build4good 2026
+            </h1>
+          </div>
+
+          <div className="absolute invisible xl:visible xl:w-[50rem] right-0 top-0">
+            <Image
+              src="/b4g/long logo.svg"
+              alt="Long Logo"
+              width={1072.48}
+              height={229.37}
+              priority
+              className="-rotate-[6.69deg] -translate-x-[10%] translate-y-[100%]"
+            />
           </div>
 
           <Image
-            src="/b4g/long logo.svg"
-            alt="Long Logo"
-            width={1072.48}
-            height={229.37}
+            src="/b4g/planet bunny.svg"
+            alt="Planet Bunny"
+            width={317.5}
+            height={232.93}
             priority
-            className="hidden xl:block xl:w-[50rem] -rotate-[6.69deg] relative right-0 top-[-30%]"
+            className="h-auto w-[20rem] absolute left-0 top-0 translate-x-[10%] translate-y-[10%]"
           />
         </div>
-
-        <Image
-          src="/b4g/planet bunny.svg"
-          alt="Planet Bunny"
-          width={317.5}
-          height={232.93}
-          priority
-          className="h-auto w-[20rem] absolute left-[5%] top-[3rem]"
-        />
-
-        <motion.div
-          style={{ x: moveBurst }}
-          className="absolute z-[2] bottom-[5%] -right-[10%] w-[60vw] h-auto"
-        >
-          <Image
-            src="/b4g/flower burst.svg"
-            alt="Flower and Burst"
-            width={605}
-            height={452}
-            className="object-contain"
-            priority
-          />
-        </motion.div>
       </section>
       <section className="h-[100dvh]">
         <div className="p-10 flex flex-col gap-3">
