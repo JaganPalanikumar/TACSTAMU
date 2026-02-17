58ef832ef1f9014dde4d100593d389040247a01f
llysi <zhaoalysa2006@gmail.com>
2026-02-17 00:58:26 -0600

    add line and +/-; get rid of weird highlighting thing

--- src/pages/b4g/components/faq/FAQItem.tsx
+++ src/pages/b4g/components/faq/FAQItem.tsx
@@ -9,9 +9,20 @@ export default function FAQItem({ question, answer }: FAQItemProps) {
   const [isOpen, setIsOpen] = useState(false);
   
   return (
-    <li className="list-none cursor-pointer mb-6" onClick={() => setIsOpen(!isOpen)}>
-      <h2 className="text-[--pink] text-4xl">{question}</h2>
-      
+    <li className="list-none cursor-pointer mb-6 pb-6 border-b border-gray last:border-0" onClick={() => setIsOpen(!isOpen)}>
+      <div 
+        className="flex justify-between items-center cursor-pointer select-none" 
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        {/* question */}
+        <h2 className="text-[--pink] text-4xl">{question}</h2>
+        
+        {/* +/- */}
+        <span className="text-[--pink] text-4xl font-mono ml-4">
+          {isOpen ? "−" : "+"}
+        </span>
+      </div>
+
       {isOpen && (
         <p className="text-2xl">{answer}</p>
       )}
