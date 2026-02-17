8926ec2d3ca03d3ca388881f903b3cd9f6c7a491
llysi <zhaoalysa2006@gmail.com>
2026-02-17 00:32:25 -0600

    change from li elements to array + mapping

--- src/pages/b4g/FAQ.tsx
+++ src/pages/b4g/FAQ.tsx
@@ -1,30 +1,25 @@
+import FAQItem from "./components/faq/FAQItem";
+
+
 export default function FAQ() {
   // TODO Fill this out
+
+  const faqs = [
+  { question: "Question 1", answer: "Answer 1" },
+  { question: "Question 2", answer: "Answer 2" },
+  { question: "Question 3", answer: "Answer 3" },
+  { question: "Question 4", answer: "Answer 4" },
+  { question: "Question 5", answer: "Answer 5" },
+];
+
   return (
     <div className="flex flex-col gap-3 p-3">
       <h1 className="w-fit mx-auto text-5xl">Frequently Asked Questions</h1>
       <div className="w-full mx-auto p-[1rem] rounded-[5rem] bg-gradient-to-b from-[--peach] to-[--pink]">
         <ul className="w-full mx-auto p-7 rounded-[4rem] bg-white text-[--gray]">
-          <li>
-            <h2 className="text-[--pink] text-5xl">Question</h2>
-            <p className="text-2xl">Answer</p>
-          </li>
-          <li>
-            <h2 className="text-[--pink] text-5xl">Question</h2>
-            <p className="text-2xl">Answer</p>
-          </li>
-          <li>
-            <h2 className="text-[--pink] text-5xl">Question</h2>
-            <p className="text-2xl">Answer</p>
-          </li>
-          <li>
-            <h2 className="text-[--pink] text-5xl">Question</h2>
-            <p className="text-2xl">Answer</p>
-          </li>
-          <li>
-            <h2 className="text-[--pink] text-5xl">Question</h2>
-            <p className="text-2xl">Answer</p>
-          </li>
+          {faqs.map((faq, index) => (
+            <FAQItem key={index} question={faq.question} answer={faq.answer} />
+          ))}
         </ul>
       </div>
     </div>
