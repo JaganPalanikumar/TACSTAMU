ae13d4d792a16a62513d56328bf8570a8322ea64
llysi <zhaoalysa2006@gmail.com>
2026-02-17 01:20:38 -0600

    make answers slide down

--- src/pages/b4g/components/faq/FAQItem.tsx
+++ src/pages/b4g/components/faq/FAQItem.tsx
@@ -12,7 +12,6 @@ export default function FAQItem({ question, answer }: FAQItemProps) {
     <li className="list-none cursor-pointer mb-6 pb-6 border-b border-gray last:border-0" onClick={() => setIsOpen(!isOpen)}>
       <div 
         className="flex justify-between items-center cursor-pointer select-none" 
-        onClick={() => setIsOpen(!isOpen)}
       >
         {/* question */}
         <h2 className="text-[--pink] text-4xl">{question}</h2>
@@ -22,10 +21,21 @@ export default function FAQItem({ question, answer }: FAQItemProps) {
           {isOpen ? "−" : "+"}
         </span>
       </div>
+      
+     {/* answer */}
+      <div 
+        className={`grid transition-all duration-200 ease-in-out ${
+          isOpen ? "grid-rows-[1fr] opacity-100 mt-4" : "grid-rows-[0fr] opacity-0 mt-0"
+        }`}
+      >
+        <div className="overflow-hidden">
 
-      {isOpen && (
-        <p className="text-2xl">{answer}</p>
-      )}
+          <p className="text-2xl cursor-default leading-relaxed">
+            {answer}
+          </p>
+        </div>
+      </div>
+      
     </li>
   );
 }
