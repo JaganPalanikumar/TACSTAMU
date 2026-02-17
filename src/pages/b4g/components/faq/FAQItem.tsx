91e1c72ad95352a942c6f32937ea1bd7993b4e2f
llysi <zhaoalysa2006@gmail.com>
2026-02-17 00:45:27 -0600

    add open/close state

--- src/pages/b4g/components/faq/FAQItem.tsx
+++ src/pages/b4g/components/faq/FAQItem.tsx
@@ -1,13 +1,20 @@
+import {useState} from "react";
+
 interface FAQItemProps {
   question: string;
   answer: string;
 }
 
 export default function FAQItem({ question, answer }: FAQItemProps) {
+  const [isOpen, setIsOpen] = useState(false);
+  
   return (
-    <li>
-      <h2 className="text-[--pink] text-5xl">{question}</h2>
-      <p className="text-2xl">{answer}</p>
+    <li className="list-none cursor-pointer mb-6" onClick={() => setIsOpen(!isOpen)}>
+      <h2 className="text-[--pink] text-4xl">{question}</h2>
+      
+      {isOpen && (
+        <p className="text-2xl">{answer}</p>
+      )}
     </li>
   );
 }
