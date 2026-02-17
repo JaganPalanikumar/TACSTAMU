0bba30e82cd08360e7b030c15bbd0b7b3e51b2a7
FunniNam3 <nguyen.ev03@gmail.com>
2026-02-17 00:23:45 -0600

    nav is done I hope and changed UI's

--- src/pages/b4g/components/navigation.tsx
+++ src/pages/b4g/components/navigation.tsx
@@ -6,7 +6,7 @@ import { motion, AnimatePresence } from "framer-motion";
 import { supabase } from "@/utils/supabase";
 
 export function Navigation() {
-  const { user, profile } = useAuth();
+  const { user, profile, logout } = useAuth();
   const [menuOpen, setMenuOpen] = useState(false);
   const [contactOpen, setContactOpen] = useState(false);
 
@@ -38,7 +38,7 @@ export function Navigation() {
   };
 
   return (
-    <nav className="relative flex w-full items-center justify-between p-5 text-[--gray] rounded-full z-50">
+    <nav className="mx-auto w-[90%] max-w-[1500] flex items-center justify-between p-5 text-black rounded-full z-50">
       <Link className="hover:scale-110 w-fit p-4" href="/b4g">
         <Image
           src="/b4g/bordered long logo.svg"
@@ -48,12 +48,12 @@ export function Navigation() {
           className="h-14 w-auto"
         />
       </Link>
-      <div className="hidden lg:flex w-fit items-center justify-between p-3 px-6 rounded-full bg-white text-center">
-        <div className="flex flex-row gap-3 text-2xl max-w-[70dvw]">
-          <Link className="p-2 hover:scale-110 my-auto" href="/b4g/Dashboard">
-            Dashboard
-          </Link>
-          {/* XXX Remove track and nav stuff atm
+      <div className="hidden max-w-[1000] flex-1 lg:flex text-2xl items-center justify-between p-3 px-6 rounded-full bg-white text-center">
+        <Link className="p-2 hover:scale-110 my-auto" href="/b4g/Dashboard">
+          Dashboard
+        </Link>
+
+        {/* XXX Remove track and nav stuff atm
           <Link className="p-2 hover:scale-110 my-auto" href="/b4g/Tracks">
             Tracks
           </Link>
@@ -72,62 +72,80 @@ export function Navigation() {
             Team Search
           </Link>
         )} */}
-          <Link className="p-2 hover:scale-110 my-auto" href="/b4g/FAQ">
-            FAQ
-          </Link>
-          <button
-            onClick={() => setContactOpen(true)}
-            className="p-2 hover:scale-110 my-auto"
-          >
-            Contact
-          </button>
+
+        <Link className="p-2 hover:scale-110 my-auto" href="/b4g/FAQ">
+          FAQ
+        </Link>
+
+        <button
+          onClick={() => setContactOpen(true)}
+          className="p-2 hover:scale-110 my-auto"
+        >
+          Contact
+        </button>
+
+        {profile?.id && (
           <Link
-            className="flex gap-5 p-2 px-5 hover:scale-110 border-[--pink] text-[--pink] border-4 rounded-full my-auto"
-            href="https://discord.gg/CBWn8mKFvx"
+            href="/b4g/User"
+            className="flex flex-row w-fit p-2 gap-3 px-5 hover:scale-110 text-black my-auto"
           >
-            <svg
-              fill="#D594DC"
-              strokeWidth="0"
-              viewBox="0 0 640 512"
-              height="2rem"
-              width="2rem"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path d="M524.531,69.836a1.5,1.5,0,0,0-.764-.7A485.065,485.065,0,0,0,404.081,32.03a1.816,1.816,0,0,0-1.923.91,337.461,337.461,0,0,0-14.9,30.6,447.848,447.848,0,0,0-134.426,0,309.541,309.541,0,0,0-15.135-30.6,1.89,1.89,0,0,0-1.924-.91A483.689,483.689,0,0,0,116.085,69.137a1.712,1.712,0,0,0-.788.676C39.068,183.651,18.186,294.69,28.43,404.354a2.016,2.016,0,0,0,.765,1.375A487.666,487.666,0,0,0,176.02,479.918a1.9,1.9,0,0,0,2.063-.676A348.2,348.2,0,0,0,208.12,430.4a1.86,1.86,0,0,0-1.019-2.588,321.173,321.173,0,0,1-45.868-21.853,1.885,1.885,0,0,1-.185-3.126c3.082-2.309,6.166-4.711,9.109-7.137a1.819,1.819,0,0,1,1.9-.256c96.229,43.917,200.41,43.917,295.5,0a1.812,1.812,0,0,1,1.924.233c2.944,2.426,6.027,4.851,9.132,7.16a1.884,1.884,0,0,1-.162,3.126,301.407,301.407,0,0,1-45.89,21.83,1.875,1.875,0,0,0-1,2.611,391.055,391.055,0,0,0,30.014,48.815,1.864,1.864,0,0,0,2.063.7A486.048,486.048,0,0,0,610.7,405.729a1.882,1.882,0,0,0,.765-1.352C623.729,277.594,590.933,167.465,524.531,69.836ZM222.491,337.58c-28.972,0-52.844-26.587-52.844-59.239S193.056,219.1,222.491,219.1c29.665,0,53.306,26.82,52.843,59.239C275.334,310.993,251.924,337.58,222.491,337.58Zm195.38,0c-28.971,0-52.843-26.587-52.843-59.239S388.437,219.1,417.871,219.1c29.667,0,53.307,26.82,52.844,59.239C470.715,310.993,447.538,337.58,417.871,337.58Z"></path>
-            </svg>
-            <h1 className="w-fit whitespace-nowrap">Join our Discord</h1>
+            <Image
+              src="/b4g/bunny icon.svg"
+              alt="bunny icon"
+              width={21.92}
+              height={31.16}
+            />
+            <span className="min-w-0 truncate">{profile.first_name}</span>
           </Link>
-          {profile?.id ? (
-            <Link
-              href="/b4g/User"
-              className="p-2 px-5 hover:scale-110 bg-[--pink] text-white rounded-full my-auto min-w-0 truncate"
-            >
-              {profile.first_name}
-            </Link>
-          ) : (
-            <Link
-              href="/b4g/Auth"
-              className="p-2 px-5 hover:scale-110 bg-[--pink] text-white rounded-full my-auto"
-            >
-              Login
-            </Link>
-          )}
-        </div>
+        )}
+
+        {profile?.id ? (
+          <button
+            onClick={logout}
+            className="p-2 px-5 hover:scale-105 bg-[--pink] text-white rounded-full my-auto"
+          >
+            Logout
+          </button>
+        ) : (
+          <Link
+            href="/b4g/Auth"
+            className="p-2 px-5 hover:scale-110 bg-[--pink] text-white rounded-full my-auto"
+          >
+            Login
+          </Link>
+        )}
+
+        <Link
+          className="flex gap-5 p-2 px-5 hover:scale-110 border-[--pink] text-[--pink] border-4 rounded-full my-auto"
+          href="https://discord.gg/CBWn8mKFvx"
+        >
+          <svg
+            fill="#D594DC"
+            strokeWidth="0"
+            viewBox="0 0 640 512"
+            height="2rem"
+            width="2rem"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path d="M524.531,69.836a1.5,1.5,0,0,0-.764-.7A485.065,485.065,0,0,0,404.081,32.03a1.816,1.816,0,0,0-1.923.91,337.461,337.461,0,0,0-14.9,30.6,447.848,447.848,0,0,0-134.426,0,309.541,309.541,0,0,0-15.135-30.6,1.89,1.89,0,0,0-1.924-.91A483.689,483.689,0,0,0,116.085,69.137a1.712,1.712,0,0,0-.788.676C39.068,183.651,18.186,294.69,28.43,404.354a2.016,2.016,0,0,0,.765,1.375A487.666,487.666,0,0,0,176.02,479.918a1.9,1.9,0,0,0,2.063-.676A348.2,348.2,0,0,0,208.12,430.4a1.86,1.86,0,0,0-1.019-2.588,321.173,321.173,0,0,1-45.868-21.853,1.885,1.885,0,0,1-.185-3.126c3.082-2.309,6.166-4.711,9.109-7.137a1.819,1.819,0,0,1,1.9-.256c96.229,43.917,200.41,43.917,295.5,0a1.812,1.812,0,0,1,1.924.233c2.944,2.426,6.027,4.851,9.132,7.16a1.884,1.884,0,0,1-.162,3.126,301.407,301.407,0,0,1-45.89,21.83,1.875,1.875,0,0,0-1,2.611,391.055,391.055,0,0,0,30.014,48.815,1.864,1.864,0,0,0,2.063.7A486.048,486.048,0,0,0,610.7,405.729a1.882,1.882,0,0,0,.765-1.352C623.729,277.594,590.933,167.465,524.531,69.836ZM222.491,337.58c-28.972,0-52.844-26.587-52.844-59.239S193.056,219.1,222.491,219.1c29.665,0,53.306,26.82,52.843,59.239C275.334,310.993,251.924,337.58,222.491,337.58Zm195.38,0c-28.971,0-52.843-26.587-52.843-59.239S388.437,219.1,417.871,219.1c29.667,0,53.307,26.82,52.844,59.239C470.715,310.993,447.538,337.58,417.871,337.58Z"></path>
+          </svg>
+          <h1 className="w-fit whitespace-nowrap">Join our Discord</h1>
+        </Link>
       </div>
 
       <button
         onClick={() => setMenuOpen(!menuOpen)}
-        className="relative flex lg:hidden flex-col justify-between items-center p-5 rounded-full bg-white aspect-square"
+        className={`relative flex lg:hidden flex-col justify-between items-center p-5 rounded-full ${menuOpen ? "bg-[--pink]" : ""} aspect-square transition duration-500`}
       >
         <div className="grid justify-items-center gap-1.5 my-auto">
           <span
-            className={`h-1 w-8 rounded-full bg-[--pink] transition duration-500 ${menuOpen ? "rotate-45" : ""} ${menuOpen ? "translate-y-2.5" : ""}`}
+            className={`h-1 w-8 rounded-full bg-white transition duration-500 ${menuOpen ? "rotate-45" : ""} ${menuOpen ? "translate-y-2.5" : ""}`}
           />
           <span
-            className={`h-1 w-8 rounded-full bg-[--pink] ${menuOpen ? "scale-x-0" : ""} transition duration-500`}
+            className={`h-1 w-8 rounded-full bg-white ${menuOpen ? "scale-x-0" : ""} transition duration-500`}
           />
           <span
-            className={`h-1 w-8 rounded-full bg-[--pink] ${menuOpen ? "-rotate-45" : ""} ${menuOpen ? "-translate-y-2.5" : ""} duration-500`}
+            className={`h-1 w-8 rounded-full bg-white transition duration-500 ${menuOpen ? "-rotate-45" : ""} ${menuOpen ? "-translate-y-2.5" : ""} duration-500`}
           />
         </div>
       </button>
@@ -169,12 +187,14 @@ export function Navigation() {
                     Team Search
                   </Link>
                 )} */}
+
                 <Link
                   className="p-2 hover:scale-105 w-fit my-auto"
                   href="/b4g/FAQ"
                 >
                   FAQ
                 </Link>
+
                 <button
                   onClick={() => {
                     setMenuOpen(false);
@@ -184,6 +204,23 @@ export function Navigation() {
                 >
                   Contact
                 </button>
+
+                {profile?.id ? (
+                  <button
+                    onClick={logout}
+                    className="p-2 px-5 hover:scale-105 w-fit bg-[--pink] text-white rounded-full my-auto min-w-0 truncate"
+                  >
+                    Logout
+                  </button>
+                ) : (
+                  <Link
+                    href="/b4g/Auth"
+                    className="p-2 px-5 hover:scale-105 w-fit bg-[--pink] text-white rounded-full my-auto"
+                  >
+                    Login
+                  </Link>
+                )}
+
                 <Link
                   className="flex gap-5 p-2 px-5 hover:scale-105 w-fit border-[--pink] text-[--pink] border-4 rounded-full my-auto"
                   href="https://discord.gg/CBWn8mKFvx"
@@ -202,21 +239,6 @@ export function Navigation() {
                     Join our Discord
                   </h1>
                 </Link>
-                {profile?.id ? (
-                  <Link
-                    href="/b4g/User"
-                    className="p-2 px-5 hover:scale-105 w-fit bg-[--pink] text-white rounded-full my-auto min-w-0 truncate"
-                  >
-                    {profile.first_name}
-                  </Link>
-                ) : (
-                  <Link
-                    href="/b4g/Auth"
-                    className="p-2 px-5 hover:scale-105 w-fit bg-[--pink] text-white rounded-full my-auto"
-                  >
-                    Login
-                  </Link>
-                )}
               </div>
             </div>
           </motion.div>
--- src/utils/supabase_types.ts
+++ src/utils/supabase_types.ts
@@ -70,34 +70,46 @@ export type Database = {
         Row: {
           created_at: string
           diet_restrictions: Json | null
+          first_hackathon: boolean | null
           first_name: string
           grad_year: number
           has_eaten: boolean
+          heard_about: string | null
+          helpful_links: string | null
           id: string
           last_name: string
           participating: boolean
+          shirt_size: string | null
           team_id: string | null
         }
         Insert: {
           created_at?: string
           diet_restrictions?: Json | null
+          first_hackathon?: boolean | null
           first_name: string
           grad_year: number
           has_eaten?: boolean
+          heard_about?: string | null
+          helpful_links?: string | null
           id: string
           last_name: string
           participating?: boolean
+          shirt_size?: string | null
           team_id?: string | null
         }
         Update: {
           created_at?: string
           diet_restrictions?: Json | null
+          first_hackathon?: boolean | null
           first_name?: string
           grad_year?: number
           has_eaten?: boolean
+          heard_about?: string | null
+          helpful_links?: string | null
           id?: string
           last_name?: string
           participating?: boolean
+          shirt_size?: string | null
           team_id?: string | null
         }
         Relationships: [
